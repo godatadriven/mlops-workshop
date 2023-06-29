8d3eecaa5c81b502af3c0c1bc6052844fc0009ae
Jeroen Overschie <jeroenoverschie@gmail.com>
2023-06-29 10:22:11 +0200

    Easier data loading and inference file

--- send_data_to_endpoint.py
+++
@@ -1,8 +0,0 @@
-import os
-import mlflow
-
-mlflow_tracking_uri = os.environ.get("MLFLOW_TRACKING_URI", "<fill in right before workshop>")
-mlflow.set_tracking_uri(mlflow_tracking_uri)
-
-mlflow.set_experiment("test_experiment")
-mlflow.log_metric("metric", 1.0)
---
+++ src/inference.py
@@ -0,0 +1,37 @@
+import mlflow
+import pandas as pd
+import plotly_express as px
+import sklearn.pipeline
+
+# Configure mlflow
+mlflow.set_tracking_uri("http://20.31.89.132:5000")
+mlflow.set_experiment("inference")
+
+# Load model
+model: sklearn.pipeline.Pipeline = mlflow.sklearn.load_model(
+    "models:/my-epic-model/production"
+)
+
+# Load data
+data_url = "https://raw.githubusercontent.com/dunnkers/experiment-tracking-with-mlflow/main/data/scada.parquet" # noqa
+data = pd.read_parquet(data_url)
+
+# Inference
+data_without_na = data.dropna()
+X = data_without_na[[
+    "wind_speed",
+    "wind_direction",
+    "is_curtailed"
+]]
+y = data_without_na["active_power"]
+data.loc[X.index, "predictions"] = model.predict(X)
+score = model.score(X, y)
+mlflow.log_metric("R2_score", score)
+
+# Plot
+plot_start = pd.Timestamp("2018-12-15")
+fig = px.line(
+    data[plot_start:],
+    y=["active_power", "predictions"]
+)
+mlflow.log_figure(fig, "predictions.html")
