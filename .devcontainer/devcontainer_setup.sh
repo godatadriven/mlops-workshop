a2fef3929f1204b93f1ffcf21561391a6eb2fea6
ykerus <yke.rusticus@gmail.com>
2023-07-14 22:20:17 +0200

    Update

--- .devcontainer/devcontainer_setup.sh
+++ .devcontainer/devcontainer_setup.sh
@@ -2,6 +2,9 @@ curl -fsSL https://get.docker.com -o get-docker.sh \
     && sh get-docker.sh \
     && rm get-docker.sh
 
+groupadd docker \
+    && usermod -aG docker vscode
+
 python -m venv .venv
 source .venv/bin/activate
 pip install ".[notebook]"
