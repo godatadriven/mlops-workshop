99d082355911bf049acf03bb5feaca319fab7bdf
ykerus <yke.rusticus@gmail.com>
2023-07-14 21:57:08 +0200

    Enable docker in docker

--- .devcontainer/devcontainer_setup.sh
+++ .devcontainer/devcontainer_setup.sh
@@ -1,3 +1,7 @@
+curl -fsSL https://get.docker.com -o get-docker.sh \
+    && sh get-docker.sh \
+    && rm get-docker.sh
+
 python -m venv .venv
 source .venv/bin/activate
 pip install ".[notebook]"
